59cd804b8b691ffd1ba6d466f2a9d4ce9a490d6e
Marek Mikula <marek@justitpro.com>
2023-04-19 21:14:46 +0200

    feat: added composable for logged in user

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>You are logged in!</div>
+  <div>You are logged in! {{ user.firstname }} {{ user.lastname }}</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUser } from '~/composables/user'
+
+const { user } = useUser()
+</script>
 
 <script lang="ts">
 export default {
