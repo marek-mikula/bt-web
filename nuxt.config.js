d7196fe4cae5d34d8209ee086dea88d017f834a4
Marek Mikula <marek@justitpro.com>
2023-04-29 20:32:39 +0200

    feat: login translated, added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,9 +34,25 @@ export default {
     ],
     link: [
       {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png'
+      },
+      {
         rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png'
+      },
+      {
+        rel: 'manifest',
+        href: '/favicon/manifest.json'
       }
     ]
   },
