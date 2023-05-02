0e60585e6c99b31e6174814ff05f4183b9c5745b
Marek Mikula <marek@justitpro.com>
2023-05-02 22:03:51 +0200

    feat: fixed design of login

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,5 +2,6 @@ import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
   theme: {},
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [require('@tailwindcss/forms')],
+  content: ['src/**/*.{ts,js,vue}', 'nuxt.config.js']
 }
