ce8ffde150fa19fa8aba314746233ae8c707e88a
Marek Mikula <marek@justitpro.com>
2023-09-01 17:46:33 +0200

    feat: page headers

--- src/pages/app/settings.vue
+++ src/pages/app/settings.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- title header -->
+    <!-- page header -->
     <div class="mb-5">
       <h2
         class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
