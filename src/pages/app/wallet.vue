b31af12e933bb44c8ed715c1f613431621d5a5a1
Marek Mikula <marek@justitpro.com>
2023-09-15 13:09:28 +0200

    feat: added title to wallet page

--- src/pages/app/wallet.vue
+++ src/pages/app/wallet.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Wallet</div>
+  <div>
+    <!-- page header -->
+    <div class="mb-5">
+      <h2
+        class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
+      >
+        {{ $t('pages.wallet.title') }}
+      </h2>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
