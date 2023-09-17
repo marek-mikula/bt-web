6c5bf928cb337c70079baa9838103e1823303939
Marek Mikula <marek@justitpro.com>
2023-09-17 15:09:12 +0200

    feat: wallet page

--- src/pages/app/wallet.vue
+++ src/pages/app/wallet.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="space-y-5">
     <!-- page header -->
-    <div class="mb-5 md:flex md:items-center md:justify-between">
+    <div class="md:flex md:items-center md:justify-between">
       <div class="min-w-0 flex-1">
         <h2
           class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
@@ -17,7 +17,6 @@
     </div>
 
     <CommonAlertOnce
-      class="mb-5"
       :identifier="'wallet-unsupported-assets'"
       :type="'info'"
       :message="$t('pages.wallet.unsupportedAssetsAlert').toString()"
@@ -83,21 +82,6 @@
               <template v-else>
                 {{ asset.currencySymbol }}
               </template>
-
-              <svg
-                v-if="!asset.isSupported"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="ml-1 h-5 w-5"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                />
-              </svg>
             </div>
 
             <CommonDropdown
