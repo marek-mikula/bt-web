e7ced5c46d31148c8fc6cdd9ebae221c0e7806ce
Marek Mikula <marek@justitpro.com>
2023-10-07 14:35:46 +0200

    feat: removed button

--- src/pages/app/cryptocurrencies/_id.vue
+++ src/pages/app/cryptocurrencies/_id.vue
@@ -28,83 +28,84 @@
                 </span>
               </h1>
             </div>
-            <div class="flex items-center gap-x-4 sm:gap-x-6">
-              <button
-                type="button"
-                class="hidden text-sm font-semibold leading-6 text-gray-900 sm:block"
-              >
-                Copy URL
-              </button>
-              <a
-                href="#"
-                class="hidden text-sm font-semibold leading-6 text-gray-900 sm:block"
-                >Edit</a
-              >
-              <a
-                href="#"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >Send</a
-              >
 
-              <div class="relative sm:hidden">
-                <button
-                  id="more-menu-button"
-                  type="button"
-                  class="-m-3 block p-3"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <span class="sr-only">More</span>
-                  <svg
-                    class="h-5 w-5 text-gray-500"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"
-                    />
-                  </svg>
-                </button>
+            <!--            <div class="flex items-center gap-x-4 sm:gap-x-6">-->
+            <!--              <button-->
+            <!--                type="button"-->
+            <!--                class="hidden text-sm font-semibold leading-6 text-gray-900 sm:block"-->
+            <!--              >-->
+            <!--                Copy URL-->
+            <!--              </button>-->
+            <!--              <a-->
+            <!--                href="#"-->
+            <!--                class="hidden text-sm font-semibold leading-6 text-gray-900 sm:block"-->
+            <!--                >Edit</a-->
+            <!--              >-->
+            <!--              <a-->
+            <!--                href="#"-->
+            <!--                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"-->
+            <!--                >Send</a-->
+            <!--              >-->
 
-                <!--
-                  Dropdown menu, show/hide based on menu state.
+            <!--              <div class="relative sm:hidden">-->
+            <!--                <button-->
+            <!--                  id="more-menu-button"-->
+            <!--                  type="button"-->
+            <!--                  class="-m-3 block p-3"-->
+            <!--                  aria-expanded="false"-->
+            <!--                  aria-haspopup="true"-->
+            <!--                >-->
+            <!--                  <span class="sr-only">More</span>-->
+            <!--                  <svg-->
+            <!--                    class="h-5 w-5 text-gray-500"-->
+            <!--                    viewBox="0 0 20 20"-->
+            <!--                    fill="currentColor"-->
+            <!--                    aria-hidden="true"-->
+            <!--                  >-->
+            <!--                    <path-->
+            <!--                      d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"-->
+            <!--                    />-->
+            <!--                  </svg>-->
+            <!--                </button>-->
 
-                  Entering: "transition ease-out duration-100"
-                    From: "transform opacity-0 scale-95"
-                    To: "transform opacity-100 scale-100"
-                  Leaving: "transition ease-in duration-75"
-                    From: "transform opacity-100 scale-100"
-                    To: "transform opacity-0 scale-95"
-                -->
-                <div
-                  class="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="more-menu-button"
-                  tabindex="-1"
-                >
-                  <!-- Active: "bg-gray-50", Not Active: "" -->
-                  <button
-                    id="more-menu-item-0"
-                    type="button"
-                    class="block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900"
-                    role="menuitem"
-                    tabindex="-1"
-                  >
-                    Copy URL
-                  </button>
-                  <a
-                    id="more-menu-item-1"
-                    href="#"
-                    class="block px-3 py-1 text-sm leading-6 text-gray-900"
-                    role="menuitem"
-                    tabindex="-1"
-                    >Edit</a
-                  >
-                </div>
-              </div>
-            </div>
+            <!--                &lt;!&ndash;-->
+            <!--                  Dropdown menu, show/hide based on menu state.-->
+
+            <!--                  Entering: "transition ease-out duration-100"-->
+            <!--                    From: "transform opacity-0 scale-95"-->
+            <!--                    To: "transform opacity-100 scale-100"-->
+            <!--                  Leaving: "transition ease-in duration-75"-->
+            <!--                    From: "transform opacity-100 scale-100"-->
+            <!--                    To: "transform opacity-0 scale-95"-->
+            <!--                &ndash;&gt;-->
+            <!--                <div-->
+            <!--                  class="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"-->
+            <!--                  role="menu"-->
+            <!--                  aria-orientation="vertical"-->
+            <!--                  aria-labelledby="more-menu-button"-->
+            <!--                  tabindex="-1"-->
+            <!--                >-->
+            <!--                  &lt;!&ndash; Active: "bg-gray-50", Not Active: "" &ndash;&gt;-->
+            <!--                  <button-->
+            <!--                    id="more-menu-item-0"-->
+            <!--                    type="button"-->
+            <!--                    class="block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900"-->
+            <!--                    role="menuitem"-->
+            <!--                    tabindex="-1"-->
+            <!--                  >-->
+            <!--                    Copy URL-->
+            <!--                  </button>-->
+            <!--                  <a-->
+            <!--                    id="more-menu-item-1"-->
+            <!--                    href="#"-->
+            <!--                    class="block px-3 py-1 text-sm leading-6 text-gray-900"-->
+            <!--                    role="menuitem"-->
+            <!--                    tabindex="-1"-->
+            <!--                    >Edit</a-->
+            <!--                  >-->
+            <!--                </div>-->
+            <!--              </div>-->
+            <!--            </div>-->
           </div>
         </div>
       </header>
